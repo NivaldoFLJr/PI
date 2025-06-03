7ef9e5e0493d6d978715c23135ca3a3f57f09048
NivaldoFLJr <nivaldof283@gmail.com>
2025-06-03 00:44:41 -0300

    fix

--- transacao.php
+++ transacao.php
@@ -8,8 +8,8 @@ $data = $_POST['data_transacao'];
 $valor = $_POST['valor'];
 $descricao = $_POST['descricao'];
 
-$sql = "INSERT INTO transacoes (id_usuario, id_conta, id_categoria, tipo_transacao, data_transacao, valor, descricao)
-        VALUES (?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO transacoes (id_categoria, tipo_transacao, data_transacao, valor, descricao)
+        VALUES (?, ?, ?, ?, ?)";
 $stmt = $OOP->prepare($sql);
 $stmt->bind_param("iiissds", $id_categoria, $tipo, $data, $valor, $descricao);
 
